579d537bd3792d49550a4e557b4ee29d1d9240bd
Щепетин Илья <shchepetin.i@dornet.ru>
2025-05-06 14:38:12 +0300

    all done

--- src/test/java/SoftAssertionsSearchShchepetinTest.java
+++ src/test/java/SoftAssertionsSearchShchepetinTest.java
@@ -1,2 +1,40 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
 public class SoftAssertionsSearchShchepetinTest {
+
+    @BeforeAll
+    static void setup() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.pageLoadStrategy = "eager";
+    }
+
+    @Test
+    void SoftAssertionsSearch() {
+        open("https://github.com/selenide/selenide");
+        $("#wiki-tab").click();
+        $("#wiki-pages-filter").setValue("SoftAssertions");
+        $("a[href='/selenide/selenide/wiki/SoftAssertions']").click();
+        $$(".markdown-heading").findBy(text("3. Using JUnit5 extend test class:"))
+                .sibling(0)
+                .$("pre")
+                .shouldHave(text(
+                        "@ExtendWith({SoftAssertsExtension.class})\n" +
+                                "class Tests {\n" +
+                                "  @Test\n" +
+                                "  void test() {\n" +
+                                "    Configuration.assertionMode = SOFT;\n" +
+                                "    open(\"page.html\");\n" +
+                                "    $(\"#first\").should(visible).click();\n" +
+                                "    $(\"#second\").should(visible).click();\n" +
+                                "  }\n" +
+                                "}"
+                ));
+
+        sleep(5000);
+    }
 }
