46fd0cd9dc91f4522dd01989139dcfe6b866e2fe
Щепетин Илья <shchepetin.i@dornet.ru>
2025-05-12 08:13:06 +0300

    delete sleep

--- src/test/java/SoftAssertionsSearchShchepetinTest.java
+++ src/test/java/SoftAssertionsSearchShchepetinTest.java
@@ -34,7 +34,5 @@ public class SoftAssertionsSearchShchepetinTest {
                                 "  }\n" +
                                 "}"
                 ));
-
-        sleep(5000);
     }
 }
