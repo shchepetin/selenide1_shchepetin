60599db14442eb8cf77c45e3b9e49e73feffd79b
Щепетин Илья <shchepetin.i@dornet.ru>
2025-05-13 12:05:25 +0300

    HoverTest Added

---
+++ src/test/java/HoverTestShchepetinTest.java
@@ -0,0 +1,25 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
+
+public class HoverTestShchepetinTest {
+    @BeforeAll
+    static void configuration() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://github.com";
+        Configuration.pageLoadStrategy = "eager";
+    }
+
+    @Test
+    void TitleTest() {
+        open("/home");
+        $(".HeaderMenu").$(withText("Solutions")).hover();
+        $$(".HeaderMenu-dropdown-link").findBy(text("Enterprises")).click();
+        $("#hero-section-brand-heading").shouldHave(text("The AI-powered developer platform"));
+    }
+}
